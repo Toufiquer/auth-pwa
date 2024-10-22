a3ffca0f49f6d202fb582c98d3f2536be922bb91
Toufiquer <toufiquerabc@gmail.com>
2024-10-22 23:11:35 +0600

    done @ pwa and working

---
+++ src/app/manifest.ts
@@ -0,0 +1,40 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Next.js PWA',
+    short_name: 'NextPWA',
+    description: 'A Progressive Web App built with Next.js',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/logo-192-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/badge-512-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ], "screenshots": [ 
+    {
+     "src": "/screenshot.png",
+      "sizes": "640x320",
+      "type": "image/png",
+      "form_factor": "wide",
+      "label": "Wonder Widgets"
+    },
+    {
+     "src": "/screenshot.png",
+      "sizes": "640x320",
+      "type": "image/png",
+      "form_factor": "narrow",
+      "label": "Wonder Widgets"
+    }
+]
+  }
+}
