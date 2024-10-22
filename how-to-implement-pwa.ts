af39348bfd156e50239056929a9c74c998dabac7
Toufiquer <toufiquerabc@gmail.com>
2024-10-22 23:19:08 +0600

    done @ how to implement pwa

---
+++ how-to-implement-pwa.ts
@@ -0,0 +1,10 @@
+/***
+ * 
+ * 1. Visit https://nextjs.org/docs/app/building-your-application/configuring/progressive-web-apps
+ * 2. implement -> app/manifest.ts
+ * 3. implement -> public/sw.js
+ * 4. update    -> next.config.js
+ * 5. add screenshot -> for mobile  and desktop 
+ * 
+ * 
+ * */ 
